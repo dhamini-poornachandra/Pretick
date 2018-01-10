7a9eeee22b8b515602aa8085a5030c653903b5c0
plank-dhamini <dhamini@plank.life>
2018-01-11 00:19:32 +0530

    #commit * Added searchBar * Added faculty dashboard activity

---
+++ app/src/main/java/com/project/msrit/pretick/FacultyDashboardActivity.java
@@ -0,0 +1,13 @@
+package com.project.msrit.pretick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacultyDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty_dashboard);
+    }
+}
---
+++ app/src/main/java/com/project/msrit/pretick/presentation/activity/SearchTicketActivity.java
@@ -0,0 +1,27 @@
+package com.project.msrit.pretick.presentation.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.SearchView;
+
+import com.project.msrit.pretick.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SearchTicketActivity extends AppCompatActivity {
+
+    @BindView(R.id.search_bar)
+    SearchView searchBar;
+
+    @BindView(R.id.search_button)
+    Button searchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_ticket);
+        ButterKnife.bind(this);
+    }
+}
